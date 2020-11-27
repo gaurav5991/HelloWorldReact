0bea8860e5b5a8270dd7faa9a1d4687d7ab5255a
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-27 18:03:40 +0530

    Gaurav[ADDED]:code to launch bridgelabz site on clicking the bridgelabz logo using event binding technique

--- helloworldapp/src/App.js
+++ helloworldapp/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import logo from './assets/bridgelabzlogo.jpg'
 class App extends React.Component {
+  url = "https://www.bridgelabz.com/"
   constructor() {
     super();
     this.state = {
@@ -9,12 +10,18 @@ class App extends React.Component {
     };
   }
 
+  // ClickHandlerFunction
+  clickHandler = ($event) =>{
+    console.log('Save Button is clicked!',$event);
+    window.open(this.url,'_blank');
+  }
+
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
         <img
-          src={logo}
+          src={logo} onClick={this.clickHandler}
           alt="The BridgeLabz logo: A Bridge to Employement through lab works"
         />
       </div>
