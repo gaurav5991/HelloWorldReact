b03613a6a84883ba53aa5c44500ee85201160db8
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-27 17:53:14 +0530

    Gaurav[ADDED]:Display BridgeLabz logo Using Property Binding

--- helloworldapp/src/App.js
+++ helloworldapp/src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
+import logo from './assets/bridgelabzlogo.jpg'
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      title : 'Hello from Bridgelabz'
-    }
+      title: "Hello from Bridgelabz",
+    };
   }
 
-    render() {
-        return ( 
-        <div>
-            <h1>{this.state.title}</h1>
-            </div>
-        );
-    }       
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <img
+          src={logo}
+          alt="The BridgeLabz logo: A Bridge to Employement through lab works"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
